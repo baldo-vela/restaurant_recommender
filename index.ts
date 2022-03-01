26f1f720670a000a85bf3add65bb6a8cff075842
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2022-03-01 02:01:18 -0600

    Adds logic to gMP

--- index.ts
+++ index.ts
@@ -3,7 +3,16 @@ import { orders, Order, PriceBracket } from "./orders";
 
 /// Add your getMaxPrice() function below:
 function getMaxPrice(filter: PriceBracket){
-
+  switch (filter) {
+    case PriceBracket.Low:
+      return "10.0";
+    case PriceBracket.Medium:
+      return "20.0";
+    case PriceBracket.High:
+      return "30.0";
+    default:
+      return "0.0";
+  }
 }
 /// Add your getOrders() function below:
 
