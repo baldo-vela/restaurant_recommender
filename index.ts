e1e077c986368b2ad6885eb7f1ed9349cd123313
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2022-03-01 01:54:22 -0600

    Adds empty getMaxPrice

--- index.ts
+++ index.ts
@@ -2,7 +2,9 @@ import { restaurants, Restaurant } from "./restaurants";
 import { orders, Order, PriceBracket } from "./orders";
 
 /// Add your getMaxPrice() function below:
+function getMaxPrice(){
 
+}
 /// Add your getOrders() function below:
 
 /// Add your printOrders() function below:
