07c26df5859edca5895c8674be92fe63c9ce85c8
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2022-03-02 01:21:01 -0600

    Fleshes out #getOrders

--- index.ts
+++ index.ts
@@ -5,20 +5,27 @@ import { orders, Order, PriceBracket } from "./orders";
 function getMaxPrice(filter: PriceBracket){
   switch (filter) {
     case PriceBracket.Low:
-      return "10.0";
+      return 10.0;
     case PriceBracket.Medium:
-      return "20.0";
+      return 20.0;
     case PriceBracket.High:
-      return "30.0";
+      return 30.0;
     default:
-      return "0.0";
+      return 0.0;
   }
 }
 /// Add your getOrders() function below:
 function getOrders(price: PriceBracket, orders: Order[][]){
-  let filteredOrders: Order[][];
+  // console.log(orders);
+  let filteredOrders: Order[][] = [];
+  orders.forEach((ordersFromRestaurant) => {
+    filteredOrders.push(
+      ordersFromRestaurant.filter((order) => order.price <= getMaxPrice(price)));
+  });
+  return filteredOrders;
 }
-
+console.log(getOrders(PriceBracket.Low, orders));
+// getOrders(PriceBracket.Low, orders);
 /// Add your printOrders() function below:
 
 /// Main
